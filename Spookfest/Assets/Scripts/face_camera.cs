8b7aacdbd59ef6104008be46c037e5f65e7d926a
Alex de la Chica <80286400+Alexdlc5@users.noreply.github.com>
2023-11-01 22:08:01 -0600

    Basic Scene

--- Spookfest/Assets/Scripts/face_camera.cs
+++ Spookfest/Assets/Scripts/face_camera.cs
@@ -7,7 +7,7 @@ public class face_camera : MonoBehaviour
 {
     public bool active = true;
     public bool is_still_object = true;
-    public float active_distance = 30;
+   // public float active_distance = 30;
     public float offset;
     private GameObject camera;
     private SpriteRenderer sr;
@@ -22,13 +22,11 @@ public class face_camera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (active && (!is_still_object || Vector2.Distance(player.transform.position, transform.position) <= active_distance))
-        {
+
             //get angle between mouse and object, rotate accordingly
             //Vector3 targetPos = camera.transform.position - transform.position;
             //float angle_yx = Mathf.Atan2(targetPos.y, -targetPos.z) * Mathf.Rad2Deg;
             //transform.rotation = Quaternion.AngleAxis(angle_yx + offset, Vector3.right);
             sr.sortingOrder = -(int)(10 * transform.position.y);
-        }
     }
 }
